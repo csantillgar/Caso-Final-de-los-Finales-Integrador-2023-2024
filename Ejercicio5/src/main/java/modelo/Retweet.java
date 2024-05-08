926de4bb813b90d9d6fd3242acc30f4fd420e2f1
csantillgar <csantill@myuax.com>
2024-05-08 10:52:07 +0200

    commit

--- Ejercicio5/src/main/java/modelo/Retweet.java
+++ Ejercicio5/src/main/java/modelo/Retweet.java
@@ -1,4 +1,20 @@
 package modelo;
 
-public class Retweet {
+import java.time.LocalDate;
+
+public class Retweet extends Tweet {
+    private Tweet tweetOriginal;
+
+    public Retweet(LocalDate fechaPublicacion, String mensaje, CuentaUsuario remitente, Tweet tweetOriginal) {
+        super(fechaPublicacion, mensaje, remitente);
+        this.tweetOriginal = tweetOriginal;
+    }
+
+    public Tweet getTweetOriginal() {
+        return tweetOriginal;
+    }
+
+    public void setTweetOriginal(Tweet tweetOriginal) {
+        this.tweetOriginal = tweetOriginal;
+    }
 }
