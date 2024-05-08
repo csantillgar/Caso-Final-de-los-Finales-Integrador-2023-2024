d288d67981f3b6ce7f3e7a65e528a01c4381b312
csantillgar <csantill@myuax.com>
2024-05-08 10:51:35 +0200

    commit

--- Ejercicio5/src/main/java/modelo/Tweet.java
+++ Ejercicio5/src/main/java/modelo/Tweet.java
@@ -1,4 +1,39 @@
 package modelo;
 
+import java.time.LocalDate;
+
 public class Tweet {
+    private LocalDate fechaPublicacion;
+    private String mensaje;
+    private CuentaUsuario remitente;
+
+    public Tweet(LocalDate fechaPublicacion, String mensaje, CuentaUsuario remitente) {
+        this.fechaPublicacion = fechaPublicacion;
+        this.mensaje = mensaje;
+        this.remitente = remitente;
+    }
+
+    public LocalDate getFechaPublicacion() {
+        return fechaPublicacion;
+    }
+
+    public void setFechaPublicacion(LocalDate fechaPublicacion) {
+        this.fechaPublicacion = fechaPublicacion;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public CuentaUsuario getRemitente() {
+        return remitente;
+    }
+
+    public void setRemitente(CuentaUsuario remitente) {
+        this.remitente = remitente;
+    }
 }
