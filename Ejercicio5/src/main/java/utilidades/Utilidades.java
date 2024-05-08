22fd75f85d54a0f92879c91e8f285e8183fbbe90
csantillgar <csantill@myuax.com>
2024-05-08 10:47:42 +0200

    commit

--- Ejercicio5/src/main/java/utilidades/Utilidades.java
+++ Ejercicio5/src/main/java/utilidades/Utilidades.java
@@ -1,4 +1,11 @@
 package utilidades;
 
 public class Utilidades {
+    public static boolean validarEmail(String email) {
+        return email != null && email.contains("@");
+    }
+
+    public static boolean validarAlias(String alias) {
+        return alias != null && alias.matches("[a-zA-Z0-9]+");
+    }
 }
