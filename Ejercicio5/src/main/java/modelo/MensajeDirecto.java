22f386d9ce4db614c01c0e8171b025855f685abe
csantillgar <csantill@myuax.com>
2024-05-08 10:53:53 +0200

    commit

--- Ejercicio5/src/main/java/modelo/MensajeDirecto.java
+++ Ejercicio5/src/main/java/modelo/MensajeDirecto.java
@@ -1,4 +1,20 @@
 package modelo;
 
-public class MensajeDirecto {
+import java.time.LocalDate;
+
+public class MensajeDirecto extends Tweet {
+    private CuentaUsuario destinatario;
+
+    public MensajeDirecto(LocalDate fechaPublicacion, String mensaje, CuentaUsuario remitente, CuentaUsuario destinatario) {
+        super(fechaPublicacion, mensaje, remitente);
+        this.destinatario = destinatario;
+    }
+
+    public CuentaUsuario getDestinatario() {
+        return destinatario;
+    }
+
+    public void setDestinatario(CuentaUsuario destinatario) {
+        this.destinatario = destinatario;
+    }
 }
